be075c520dfe413ea398e4656dc43de16f1dfee6
tianenGit <415258758@qq.com>
2022-08-20 22:58:27 +0800

    OJ

--- 8-15_OJ-CodeDay-week11/autumn.cpp
+++ 8-15_OJ-CodeDay-week11/autumn.cpp
@@ -1,102 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-class MyCircularDeque {
-public:
-    vector<int> q;
-    int hh = 0, tt = 0;
-
-    MyCircularDeque(int k) {
-        q.resize(k + 1);
-    }
-
-    int get(int x) {
-        return (x + q.size()) % q.size();
-    }
-
-    bool insertFront(int value) {
-        if (isFull()) return false;
-        hh = get(hh - 1);
-        q[hh] = value;
-        return true;
-    }
-
-    bool insertLast(int value) {
-        if (isFull()) return false;
-        q[tt] = value;
-        tt = get(tt + 1);
-        return true;
-    }
-
-    bool deleteFront() {
-        if (isEmpty()) return false;
-        hh = get(hh + 1);
-        return true;
-    }
-
-    bool deleteLast() {
-        if (isEmpty()) return false;
-        tt = get(tt - 1);
-        return true;
-    }
-
-    int getFront() {
-        if (isEmpty()) return -1;
-        return q[hh];
-    }
-
-    int getRear() {
-        if (isEmpty()) return -1;
-        return q[get(tt - 1)];
-    }
-
-    bool isEmpty() {
-        return hh == tt;
-    }
-
-    bool isFull() {
-        return tt == get(hh - 1);
-    }
-};
-
-class Solution {
-public:
-
-    int deepestLeavesSum(TreeNode* root) {
-        queue<TreeNode*>q;
-
-        q.push(root);
-        int ans = 0;
-        while (!q.empty())
-        {
-            int len = q.size();
-            ans = 0;
-            while (len--)
-            {
-                TreeNode* t = q.front();
-                q.pop();
-                ans += t->val;
-                if (t->left)
-                    q.push(t->left);
-                if (t->right)
-                    q.push(t->right);
-
-
-            }
-
-        }
-        return ans;
-    }
-};
-
-
-
-class Solution {
-public:
-    int busyStudent(vector<int>& st, vector<int>& et, int q) {
-        int res = 0;
-        for (int i = 0; i < st.size(); i++) {
-            if (st[i] <= q && et[i] >= q)res++;
-        }
-        return res;
-    }
-};
+/*1450. 在既定时间做作业的学生人数*/
+/* https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/ */
+//class Solution {
+//public:
+//    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+//        const int N = 1011;
+//        int sum[N] = { 0 };
+//
+//        for (auto x : startTime)
+//            sum[x]++;
+//        for (auto x : endTime)
+//            sum[x + 1]--;
+//
+//        for (int i = 1; i < N; i++)
+//            sum[i] += sum[i - 1];
+//        return sum[queryTime];
+//    }
+//};
