47878301fc60099079552ea8fcac21ff9d9681ad
tianenGit <415258758@qq.com>
2022-10-02 21:43:03 +0800

    C++Note

---
+++ 10-2_TestNote/test.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void fun(int arr[])
+{
+
+}
+
+struct n1
+{
+	int a;
+};
+class n2
+{
+	int a;
+};
+
+
+int main()
+{
+	/*int a = 10;
+
+	int& ra = a;
+	const double& rra = (double)a;*/
+
+	n1 _n1;
+	_n1.a = 10;
+
+	n2 _n2;
+	//_n2.a = 10;
+
+	return 0;
+}
