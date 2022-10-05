d39f1400c1314c23c61137029a835205e01f3fa1
tianenGit <415258758@qq.com>
2022-10-05 23:40:35 +0800

    Test

--- 7-11_TestWork/Test.cpp
+++ 7-11_TestWork/Test.cpp
@@ -100,19 +100,33 @@ using namespace std;
 //}
 
 
-int main()
-{
-	int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
-	int n = sizeof(ar) / sizeof(int);
-	vector<int> v(ar, ar + n);
-	vector<int>::iterator it = v.begin();
-	while (it != v.end())
-	{
-		if (*it != 0)
-			cout << *it;
-		else
-			v.erase(it);
-		it++;
-	}
-	return 0;
-}
+//int main()
+//{
+//	int ar[] = { 1,2,3,4,0,5,6,7,8,9 };
+//	int n = sizeof(ar) / sizeof(int);
+//	vector<int> v(ar, ar + n);
+//	vector<int>::iterator it = v.begin();
+//	while (it != v.end())
+//	{
+//		if (*it != 0)
+//			cout << *it;
+//		else
+//			v.erase(it);
+//		it++;
+//	}
+//	return 0;
+//}
+
+//struct A
+//{
+//	char c;
+//	int arr[0];
+//};
+//
+//int main()
+//{
+//	
+//	cout << sizeof(struct A);
+//
+//	return 0;
+//}
