ab3d3f68516b593d369a4bbe3ca21a3350442e9b
tianenGit <415258758@qq.com>
2022-10-09 22:03:55 +0800

    C_test

---
+++ 7-11_TestWork/Clanguage.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//struct A
+//{
+//	int c;
+//	int arr[0];
+//};
+//
+//int main()
+//{
+//	
+//	printf("%d", sizeof(struct A));
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int i = 0;
+//
+//int main()
+//{
+//	printf("%d %d %d", (i++) , (++i) , (i++));
+//
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+	printf("%s %.3s\n", "computer", "computer");
+
+	printf("%.1f a", 123.00);
+
+	return 0;
+}
