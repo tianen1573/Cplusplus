a7760f69f3396b2fa71e0e83247078bd31cc84ec
tianenGit <415258758@qq.com>
2023-04-05 13:25:49 +0800

    复习

--- 10-2_TestNote/test.cpp
+++ 10-2_TestNote/test.cpp
@@ -109,15 +109,41 @@ using namespace std;
 //	auto t = 1;
 //};
 
+//int main()
+//{
+//	/*test<int> t;*/
+//
+//	const vector<int> cvec;
+//	const list<int> cl;
+//
+//	vector<int>::const_iterator it = cvec.begin();
+//	list<int>::const_iterator it2 = cl.begin();
+//
+//	return 0;
+//}
+
+template<class T>
+T Add(const T& left, const T& right)
+{
+    return left + right;
+}
 int main()
 {
-	/*test<int> t;*/
+    int a1 = 10, a2 = 20;
+    double d1 = 10.0, d2 = 20.0;
+    Add(a1, a2);
+    Add(d1, d2);
 
-	const vector<int> cvec;
-	const list<int> cl;
+    /*
+ 该语句不能通过编译，因为在编译期间，当编译器看到该实例化时，需要推演其实参类型
+ 通过实参a1将T推演为int，通过实参d1将T推演为double类型，但模板参数列表中只有一个T，
+ 编译器无法确定此处到底该将T确定为int 或者 double类型而报错
+ 注意：在模板中，编译器一般不会进行类型转换操作，因为一旦转化出问题，编译器就需要背黑锅
+ Add(a1, d1);
+ */
 
-	vector<int>::const_iterator it = cvec.begin();
-	list<int>::const_iterator it2 = cl.begin();
-
-	return 0;
+ // 此时有两种处理方式：1. 用户自己来强制转化 2. 使用显式实例化
+    Add(a1, (int)d2);
+    Add<int>(a1, d2);
+    return 0;
 }
