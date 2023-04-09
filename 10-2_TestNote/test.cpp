7a47b1f9368f8ac6803c052ed1d0645ff4737155
tianenGit <415258758@qq.com>
2023-04-09 14:12:15 +0800

    复习

--- 10-2_TestNote/test.cpp
+++ 10-2_TestNote/test.cpp
@@ -122,28 +122,225 @@ using namespace std;
 //	return 0;
 //}
 
-template<class T>
-T Add(const T& left, const T& right)
+//template<class T>
+//T Add(const T& left, const T& right)
+//{
+//    return left + right;
+//}
+//int main()
+//{
+//    int a1 = 10, a2 = 20;
+//    double d1 = 10.0, d2 = 20.0;
+//    Add(a1, a2);
+//    Add(d1, d2);
+//
+//    /*
+// 该语句不能通过编译，因为在编译期间，当编译器看到该实例化时，需要推演其实参类型
+// 通过实参a1将T推演为int，通过实参d1将T推演为double类型，但模板参数列表中只有一个T，
+// 编译器无法确定此处到底该将T确定为int 或者 double类型而报错
+// 注意：在模板中，编译器一般不会进行类型转换操作，因为一旦转化出问题，编译器就需要背黑锅
+// Add(a1, d1);
+// */
+//
+// // 此时有两种处理方式：1. 用户自己来强制转化 2. 使用显式实例化
+//    Add(a1, (int)d2);
+//    Add<int>(a1, d2);
+//    return 0;
+//}
+
+#include<assert.h>
+
+class Car {
+public:
+    virtual void Drive() {}
+};
+class Benz :public Car {
+public:
+    virtual void Drive() override { cout << "Benz-舒适" << endl; }
+    virtual void Drive(int a) { cout << "Benz-舒适" << endl; }
+
+};
+
+namespace bit
 {
-    return left + right;
+    class string
+    {
+    public:
+        typedef char* iterator;
+        iterator begin()
+        {
+            return _str;
+        }
+        iterator end()
+        {
+            return _str + _size;
+        }
+        string(const char* str = "")
+            :_size(strlen(str))
+            , _capacity(_size)
+        {
+            //cout << "string(char* str)" << endl;
+            _str = new char[_capacity + 1];
+            strcpy(_str, str);
+        }
+        // s1.swap(s2)
+        void swap(string& s)
+        {
+            ::swap(_str, s._str);
+            ::swap(_size, s._size);
+            ::swap(_capacity, s._capacity);
+        }
+        // 拷贝构造
+        string(const string& s)
+            :_str(nullptr)
+        {
+            cout << "string(const string& s) -- 深拷贝" << endl;
+            string tmp(s._str);
+            swap(tmp);
+        }
+        // 赋值重载
+        string& operator=(const string& s)
+        {
+            cout << "string& operator=(string s) -- 深拷贝" << endl;
+            string tmp(s);
+            swap(tmp);
+            return *this;
+        }
+        // 移动构造
+        string(string&& s)
+            :_str(nullptr)
+            , _size(0)
+            , _capacity(0)
+        {
+            cout << "string(string&& s) -- 移动语义" << endl;
+            swap(s);
+        }
+        // 移动赋值
+        string& operator=(string&& s)
+        {
+            cout << "string& operator=(string&& s) -- 移动语义" << endl;
+            swap(s);
+            return *this;
+        }
+        ~string()
+        {
+            delete[] _str;
+            _str = nullptr;
+        }
+        char& operator[](size_t pos)
+        {
+            assert(pos < _size);
+            return _str[pos];
+        }
+        void reserve(size_t n)
+        {
+            if (n > _capacity)
+            {
+                char* tmp = new char[n + 1];
+                strcpy(tmp, _str);
+                delete[] _str;
+                _str = tmp;
+                _capacity = n;
+            }
+        }
+        void push_back(char ch)
+        {
+            if (_size >= _capacity)
+            {
+                size_t newcapacity = _capacity == 0 ? 4 : _capacity * 2;
+                reserve(newcapacity);
+            }
+            _str[_size] = ch;
+            ++_size;
+            _str[_size] = '\0';
+        }
+        //string operator+=(char ch)
+        string& operator+=(char ch)
+        {
+            push_back(ch);
+            return *this;
+        }
+        const char* c_str() const
+        {
+            return _str;
+        }
+    private:
+        char* _str;
+        size_t _size;
+        size_t _capacity; // 不包含最后做标识的\0
+    };
 }
+
+template<class T>
+struct ListNode
+{
+    ListNode* _next = nullptr;
+    ListNode* _prev = nullptr;
+    T _data;
+};
+template<class T>
+class List
+{
+    typedef ListNode<T> Node;
+public:
+    List()
+    {
+        _head = new Node;
+        _head->_next = _head;
+        _head->_prev = _head;
+    }
+    void PushBack(T&& x)
+    {
+        //Insert(_head, x);
+        Insert(_head, std::forward<T>(x));
+    }
+    void PushFront(T&& x)
+    {
+        //Insert(_head->_next, x);
+        Insert(_head->_next, std::forward<T>(x));
+    }
+    void Insert(Node* pos, T&& x)
+    {
+        Node* prev = pos->_prev;
+        Node* newnode = new Node;
+        newnode->_data = std::forward<T>(x); // 关键位置
+        // prev newnode pos
+        prev->_next = newnode;
+        newnode->_prev = prev;
+        newnode->_next = pos;
+        pos->_prev = newnode;
+    }
+    void Insert(Node* pos, const T& x)
+    {
+        Node* prev = pos->_prev;
+        Node* newnode = new Node;
+        newnode->_data = x; // 关键位置
+        // prev newnode pos
+        prev->_next = newnode;
+        newnode->_prev = prev;
+        newnode->_next = pos;
+        pos->_prev = newnode;
+    }
+private:
+    Node* _head;
+};
 int main()
 {
-    int a1 = 10, a2 = 20;
-    double d1 = 10.0, d2 = 20.0;
-    Add(a1, a2);
-    Add(d1, d2);
-
-    /*
- 该语句不能通过编译，因为在编译期间，当编译器看到该实例化时，需要推演其实参类型
- 通过实参a1将T推演为int，通过实参d1将T推演为double类型，但模板参数列表中只有一个T，
- 编译器无法确定此处到底该将T确定为int 或者 double类型而报错
- 注意：在模板中，编译器一般不会进行类型转换操作，因为一旦转化出问题，编译器就需要背黑锅
- Add(a1, d1);
- */
-
- // 此时有两种处理方式：1. 用户自己来强制转化 2. 使用显式实例化
-    Add(a1, (int)d2);
-    Add<int>(a1, d2);
+    List<bit::string> lt;
+    lt.PushBack("1111");
+    lt.PushFront("2222");
     return 0;
 }
+
+//int main()
+//{
+//    int&& rr1  = 10;
+//
+//    auto ptr = &rr1;
+//
+//    *ptr = 11;
+//
+//    cout << *ptr << ' ' << rr1;
+//
+//    return 0;
+//}
