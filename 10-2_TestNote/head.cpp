60ac2f5be5f9b525ff883d6a2c4cfa5e05309be3
tianenGit <415258758@qq.com>
2023-07-12 20:40:21 +0800

    补充图片

---
+++ 10-2_TestNote/head.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "head.h"
+
+//void func3()
+//{
+//	cout << "inline \n";
+//}
---
+++ 10-2_TestNote/head.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <list>
+#include <Windows.h>
+#include <mutex>
+
+using namespace std;
+
+//inline void func3();
